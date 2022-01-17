f9484e4f85e3b9c4ff8129cc1c989f7df7600f3f
philmillians <philmillians@yahoo.com>
2022-01-17 17:01:45 -0500

    modified Array.py and hashTable.py

--- Arrays/Array.py
+++ Arrays/Array.py
@@ -1,30 +1,42 @@
+INITIAL_CAPACITY=16
 class ArrayList:
-  def __init__(self,value=None):
-    self.size = value
+  def __init__(self,size):
+    self.size = 0
+    self.capacity=INITIAL_CAPACITY
     self.data = {}
-
   def __str__(self):
     return str(self.__dict__)
-
   def access(self,index):
     if index > self.size:
       print("index out of range")
       return
-    print(self.data[index])
     return self.data[index]
-  def search(self,value):
-    for item in self.data:
-      if self.data[item] == value:
-        print(f'Found {value} at index: {item}')
-        return
-  def delete(self,value):
-    deletedItem = self.search(value)
-    previous
-    
+  def insertAtIndex(self,index, newValue):
+    self.data[index]=newValue
+    self.size +=1
+    return
+  def insertAtEnd(self,newValue):
+    self.data[self.size]=newValue
+    self.size +=1
+    return
   def insert(self,newValue,index):
     self.data[index]=newValue
     self.size += 1
     return
+  # def insertAtBeginning(self,newValue):
+  #   for item in self.data:
+  #     index += 
+  #   return self.data[0]=newValue
+  #   self.size +=1
+  def search(self,value):
+    for item in range(self.size):
+      if self.data[item]==value:
+        print(f'Found {value} at index: {item}')
+        return self.data[item]
+  def delete(self,value):
+    del self.data[value] 
+    self.size -=1
+  
   def myLen(self):
     print(self.size)
     return self.size
--- HashTable/hashTable.py
+++ HashTable/hashTable.py
@@ -1,75 +1,64 @@
-INITIAL_CAPACITY = 16   
 class Node:
   def __init__(self,key,value=None):
-    self.key = key
-    self.value = value
-    self.next = None
-  
-  def __str__(self):
-    return "<Node: (%s, %s), next: %s>" % (self.key, self.value, self.next != None)
-  
-  def __repr__(self):
-    return str(self)
-
-class HashTable():
-  def __init__(self):
-    self.capacity = INITIAL_CAPACITY
-    self.buckets = [None]*self.capacity
     self.size = 0
-  
+    self.value=value
+    self.key = key
+class HashTable:
+  INITIAL_CAPACITY=16
+
+  def __init__(self):
+    self.size=0
+    self.capacity=16
+    self.buckets= [None]*self.capacity
   def __str__(self):
     return str(self.__dict__)
-  
-  def hash(self, key):
-    hashsum = 0
-    for index, c in enumerate(key):
-  # Add (index + length of key) ^ (current char code)
-      hashsum += (index + len(key)) ** ord(c)
-  # Perform modulus to keep hashsum in range [0, self.capacity - 1]
-      hashsum = hashsum % self.capacity
-    return hashsum
-  
-  def find(self, value):
-    index = self.hash(key)
-    node = self.buckets[index]
-    while (node != None and node.key != key):
-      node = node.next
-    if node == None:
-      print("Key not in HashTable")
-      return None
-    return node.value
-  
-  def add(self, key, value):
-    self.size += 1
+  def insert(self,key,value):
+    self.size +=1
     NewNode = Node(key,value)
     index = self.hash(key)
     node = self.buckets[index]
-    if node is None:
-      self.buckets[index] = NewNode
+    if node ==None:
+      self.buckets[index]=NewNode
       return
     previous = node
-    while node is not None:
+    while node:
       previous = node
       node = node.next
     previous.next = NewNode
 
-  def remove(self, key):
-    self.size -= 1
-  
-  def remove(self, key):
-    index = self.hash(key)
-    node = self.buckets[index]
+  def remove(self,key):
     previous = None
-    while node is not None and node.key != key:
+    index = self.hash(key)
+    node=self.buckets[index]
+    #previous = None
+    
+    while node and node.key != key:
       previous = node
       node = node.next
-    if node is None:
+    if node == None:
       return None
     else:
-      self.size -= 1
+      self.size -=1
       result = node.value
-      if previous is None:
-        self.buckets[index] = node.next 
+      if previous == None:
+        node=None
       else:
-        previous.next = prev.next.next 
-        return result 
+        previous.next = previous.next.next
+  def hash(self,key):
+    hashsum = 0
+    for index, c in enumerate(key):
+      hashsum += (index + len(key)) ** ord(c)
+      hashsum = hashsum % self.capacity
+    return hashsum
+
+  def search(self,value):
+    index = self.hash(key)
+    node=self.buckets[index]
+    while(node != None and node.key != key):
+      node=node.next
+    if node == None:
+      print('key not in hashtable')
+      return None
+    return node.value
+  def access(self,value):
+    pass
